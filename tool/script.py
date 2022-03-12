1594ce155754f45acac68f0e339238cd20310a4e
Akilax0 <killerilaksha@gmail.com>
2022-03-12 19:02:20 +0530

    AB#25 script reading and output

--- tool/script.py
+++ tool/script.py
@@ -1,9 +1,5 @@
 import csv
 
-
-
-
-
 def init():
     # setting parameters
     print("Setting parameters...")
@@ -12,28 +8,26 @@ def read(tot, pos):
     print("Reading output file")
     with open('cities.csv', newline='') as csvfile:
         spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-   
+    
+        #skip heading
         next(spamreader)
 
         for row in spamreader:
-            
             tot+=1
-            print(' '.join(row))
-            if row[-1].type == float and  float(row[-1]) > 125:
+            # print(' '.join(row))
+            if len(row[-1]) > 0 and float(row[-1]) > 125:    
+                # print(type(float(row[-1])))
                 pos+=1
-
+    return tot,pos
 
 def out(tot,pos):
-    print("Total mold shots in data set: {} \n success rate: {} \n", tot,pos)
+    print("Total mold shots in data set: {} \n success rate: {} \n".format(tot,pos/tot*100))
     
-            
-
-
-
 if __name__ == "__main__":
-    tot = 0
-    pos = 0
 
-    read(tot,pos)
+    tot = 0
+    pos=0
+
+    tot,pos = read(tot,pos)
 
     out(tot,pos)
