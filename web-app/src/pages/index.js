a2877423dfa49b56fecf2638a0f29d121e28a783
Dinura Dissanayake <dinuraransikadissanayake@gmail.com>
2022-03-13 19:44:40 +0530

    Basic layout

---
+++ web-app/src/pages/index.js
@@ -0,0 +1,29 @@
+import Head from 'next/head';
+import { Box, Container, Grid } from '@mui/material';
+import { DashboardLayout } from '../components/dashboard-layout';
+
+const Dashboard = () => (
+  <>
+    <Head>
+      <title>
+        Dashboard | Material Kit
+      </title>
+    </Head>
+    <Box
+      component="main"
+      sx={{
+        flexGrow: 1,
+        py: 8
+      }}
+    >
+    </Box>
+  </>
+);
+
+Dashboard.getLayout = (page) => (
+  <DashboardLayout>
+    {page}
+  </DashboardLayout>
+);
+
+export default Dashboard;
