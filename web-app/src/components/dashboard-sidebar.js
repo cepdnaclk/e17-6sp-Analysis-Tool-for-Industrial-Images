928f9c179db9cfc8e5ddd83c133f1f261f190551
Dinura Dissanayake <dinuraransikadissanayake@gmail.com>
2022-03-16 10:05:42 +0530

    Changes to the side panel

--- web-app/src/components/dashboard-sidebar.js
+++ web-app/src/components/dashboard-sidebar.js
@@ -4,21 +4,15 @@ import { useRouter } from 'next/router';
 import PropTypes from 'prop-types';
 import { Box, Button, Divider, Drawer, Menu, Typography, useMediaQuery } from '@mui/material';
 
-import OpenInNewIcon from '@mui/icons-material/OpenInNew';
 import { ChartBar as ChartBarIcon } from '../icons/chart-bar';
-import { Cog as CogIcon } from '../icons/cog';
-import { Lock as LockIcon } from '../icons/lock';
-import { Selector as SelectorIcon } from '../icons/selector';
 import { ShoppingBag as ShoppingBagIcon } from '../icons/shopping-bag';
 import { User as UserIcon } from '../icons/user';
-import { UserAdd as UserAddIcon } from '../icons/user-add';
-import { Users as UsersIcon } from '../icons/users';
-import { XCircle as XCircleIcon } from '../icons/x-circle';
+import { LogOut as LogOutIcon } from '../icons/log-out';
 
 import { Logo } from './logo';
 import { NavItem } from './nav-item';
 
-const items = [
+const Navitems = [
   {
     href: '/',
     icon: (<ChartBarIcon fontSize="small" />),
@@ -28,6 +22,19 @@ const items = [
     href: '/',
     icon: (<ShoppingBagIcon fontSize="small" />),
     title: 'Production Data'
+  }
+];
+
+const items = [
+  {
+    href: '/',
+    icon: (<UserIcon fontSize="small" />),
+    title: 'Profile'
+  },
+  {
+    href: '/',
+    icon: (<LogOutIcon fontSize="small" />),
+    title: 'Logout'
   }
 ];
 
@@ -109,12 +116,12 @@ export const DashboardSidebar = (props) => {
           }}
         />
         <Box sx={{ flexGrow: 1 }}>
-          {items.map((item) => (
+          {Navitems.map((Navitem) => (
             <NavItem
-              key={item.title}
-              icon={item.icon}
-              href={item.href}
-              title={item.title}
+              key={Navitem.title}
+              icon={Navitem.icon}
+              href={Navitem.href}
+              title={Navitem.title}
             />
           ))}
         </Box>
@@ -125,7 +132,16 @@ export const DashboardSidebar = (props) => {
             py: 3
           }}
         >
-          
+          <Box sx={{ flexGrow: 1 }}>
+            {items.map((item) => (
+              <NavItem
+                key={item.title}
+                icon={item.icon}
+                href={item.href}
+                title={item.title}
+              />
+            ))}
+          </Box>
         
         </Box>
       </Box>
