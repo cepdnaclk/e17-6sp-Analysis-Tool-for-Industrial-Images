58dbefaf9050cf22cf03abee359d8894740169c6
shamalweerasooriya <e17380@eng.pdn.ac.lk>
2022-03-27 09:24:57 +0530

    resolve issues in database

--- server/database/analysis_tool.sql
+++ server/database/analysis_tool.sql
@@ -2,26 +2,30 @@ CREATE DATABASE analysis_tool;
 
 USE analysis_tool;
 
-CREATE TABLE analysis_tool.employee(
+CREATE TABLE analysis_tool.employees(
     empID  VARCHAR(10) PRIMARY KEY,
     firstName VARCHAR(255) NOT NULL,
     lastName VARCHAR(255) NOT NULL,
     empTitle VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
     admin BIT NOT NULL
 );
 
-CREATE TABLE analysis_tool.mold(
+CREATE TABLE analysis_tool.passwords(
+    empID VARCHAR(10) PRIMARY KEY,
+    password VARCHAR(255) NOT NULL,
+    FOREIGN KEY (empID) REFERENCES analysis_tool.employees(empID)
+);
+CREATE TABLE analysis_tool.molds(
     moldID  VARCHAR(10) PRIMARY KEY,
     monaNumber VARCHAR(255) NOT NULL,
     material VARCHAR(255) NOT NULL,
     moldMaker VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE analysis_tool.machine(
+CREATE TABLE analysis_tool.machines(
     machineID VARCHAR(10) PRIMARY KEY,
     moldID VARCHAR(10) NOT NULL,
-    FOREIGN KEY (moldID) REFERENCES analysis_tool.mold(moldID),
+    FOREIGN KEY (moldID) REFERENCES analysis_tool.molds(moldID),
     moldShots INT NOT NULL,
     failedShots INT NOT NULL,
     prodRate INT NOT NULL,
