64b8f48bd33cd4454ef78117b1c155fc9a8e57a0
akilax0 <killerilaksha@gmail.com>
2022-06-20 10:28:10 +0530

    post request + simulation tool

--- server/APIs/app.js
+++ server/APIs/app.js
@@ -7,6 +7,8 @@ var app = express();
 //Configuring express server
 app.use(bodyparser.json());
 
+app.use(bodyparser.urlencoded({ extended: true }));
+
 // connection configurations
 const db = mysql.createConnection({
     host: 'localhost',
@@ -30,13 +32,36 @@ app.get("/api/machines" , (req,res)=>{
 });
 
 //get selected machine details
-app.get("/api/machines/:machine_id" , (req,res)=>{
+app.route("/api/machines/:machine_id")
+	.get((req,res)=>{
 	const machine_id = req.params.machine_id;
 	const sqlMachines = "SELECT * FROM machines where machineID = ? ;"
 	db.query(sqlMachines,machine_id,(err,result)=>{
 		res.send(result);
 	})
-});
+	})
+	.post((req,res)=>{
+		var jsonStr = JSON.stringify(req.body);
+		console.log(jsonStr);
+
+		res.send(test)
+	
+		// //res.json({requestBody: req.body})
+		// console.log(req.body[0].machine_id)
+		// //res.sendStatus(200);
+
+		// const machine_id = req.body.machine_id;
+		// const failedShots = req.body.failedShots;
+
+		// const sqlMachines = "UPDATE machines SET failedShots = 9 WHERE machineID = 'D02'; "
+		// db.query(sqlMachines,machine_id,(err,result)=>{
+		// 	if(err)throw err;
+		// 	res.send(result);
+		// 	console.log(result);
+		// 	console.log("update");
+		// })
+		//res.send('POST REQUEST SUCCESS');
+	});
 
 
 //get all mold details
@@ -48,13 +73,19 @@ app.get("/api/molds" , (req,res)=>{
 });
 
 //get selected mold details
-app.get("/api/molds/:mold_id" , (req,res)=>{
-	const mold_id = req.params.mold_id;
-	const sqlMolds = "SELECT * FROM molds where moldID = ? ;"
-	db.query(sqlMolds,mold_id,(err,result)=>{
-		res.send(result);
+app.route("/api/molds/:mold_id")
+	.get((req,res)=>{
+		const mold_id = req.params.mold_id;
+		const sqlMolds = "SELECT * FROM molds where moldID = ? ;"
+		db.query(sqlMolds,mold_id,(err,result)=>{
+			res.send(result);
+		})
 	})
-});
+	.post((req,res)=>{
+		res.send('POST REQUEST')
+	});
+	
+
 
 
 // start the server and connect to the database in it
